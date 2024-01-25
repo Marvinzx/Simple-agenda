cbacbcedffe65cb794bac31bf4d5bbc0efda240e
trabajo 1 <mh23-0285@unphu.edu.do>
2024-01-24 21:08:32 -0400

    primera version de aplicacion

---
+++ SimpleAgenda.java
@@ -0,0 +1,23 @@
+public class SimpleAgenda {
+    public static void agregarContacto (String a){
+
+    }
+
+    public static void removerContacto(int b){
+
+    }
+    public  static String acualizarContacto(int N)
+    {
+        return "N/A";
+
+    }
+    public static void mostrarContactos(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
